b98a0d960458bcc12b65bb422339df896118680d
GretaABazzno <silmari83@gmail.com>
2019-05-17 12:44:29 +0200

    Initi new project

---
+++ src/factory.java
@@ -0,0 +1,14 @@
+public class CFactory {
+	
+	private String mfactoryType;
+	
+	public CFactory() {
+		
+		
+	}
+	
+	public static void main(String[] args) {
+			System.out.println("hello world");
+	}
+	 	
+}
